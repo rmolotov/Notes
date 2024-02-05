b3ea71f0f9af6a3d66fdf63d2cab287cea5197d6
Roman Molotov <r.molotov@yahoo.com>
2024-02-05 22:09:49 +0400

    [IdentityServer] add storage migrations

--- Infrastructure/Identity/Notes.Identity/Startup.cs
+++ Infrastructure/Identity/Notes.Identity/Startup.cs
@@ -2,13 +2,18 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System.Linq;
+using System.Reflection;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Notes.Identity.Data;
 using Notes.Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,29 +34,53 @@ namespace Notes.Identity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("UsersConnection")));
 
-            // services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            var builder = services.AddIdentityServer(options =>
-            {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
+            var builder = services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
 
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
+                    // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(
+                            Configuration.GetConnectionString("IdentityServer"),
+                            sql =>
+                            {
+                                sql.MigrationsAssembly(migrationsAssembly);
+                                sql.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                            })
+                        .ConfigureWarnings(warnings =>
+                            warnings.Ignore(CoreEventId.RowLimitingOperationWithoutOrderByWarning));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(
+                            Configuration.GetConnectionString("IdentityServer"),
+                            sql =>
+                            {
+                                sql.MigrationsAssembly(migrationsAssembly);
+                                sql.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                            })
+                        .ConfigureWarnings(warnings =>
+                            warnings.Ignore(CoreEventId.RowLimitingOperationWithoutOrderByWarning));
+                })
                 .AddAspNetIdentity<ApplicationUser>();
 
             // not recommended for production - you need to store your key material somewhere secure
@@ -72,10 +101,12 @@ namespace Notes.Identity
 
         public void Configure(IApplicationBuilder app)
         {
+            InitializeDatabase(app);
+            
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                // app.UseMigrationsEndPoint();
+                app.UseMigrationsEndPoint();
             }
 
             app.UseStaticFiles();
@@ -87,6 +118,43 @@ namespace Notes.Identity
             {
                 endpoints.MapDefaultControllerRoute();
             });
+        }
+        
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                // context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
