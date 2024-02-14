f8eff22b74813d0e3a306aa6a190c2708f9fdee0
Roman Molotov <r.molotov@yahoo.com>
2024-02-14 19:54:03 +0400

    add mssql healthcheck in docker compose

--- Infrastructure/Identity/Notes.Identity/Startup.cs
+++ Infrastructure/Identity/Notes.Identity/Startup.cs
@@ -10,11 +10,13 @@ using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using UserManagement.Core.Entities;
 using UserManagement.Persistence.Database;
 
@@ -101,10 +103,11 @@ namespace Notes.Identity
 
         public void Configure(IApplicationBuilder app)
         {
-            // InitializeDatabase(app); // uncomment for add migrations and scopes/clients to ext db
+            InitializeDatabase(app); // uncomment for add migrations and scopes/clients to ext db
             
             if (Environment.IsDevelopment())
             {
+                Log.Information("is development");
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
@@ -122,38 +125,51 @@ namespace Notes.Identity
         
         private void InitializeDatabase(IApplicationBuilder app)
         {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            Log.Information("Database initialization...");
+            
+            using var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>()
+                .CreateScope();
+            
+            var persistedContext = serviceScope.ServiceProvider
+                .GetRequiredService<PersistedGrantDbContext>();
+
+            var configContext = serviceScope.ServiceProvider
+                .GetRequiredService<ConfigurationDbContext>();
+
+            try
             {
-                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                persistedContext.Database.Migrate();
+                configContext.Database.Migrate();
+            }
+            catch (SqlException exception) when (exception.ErrorCode == 1801)
+            {
+                Log.Warning("Database exists, skip initialization");
+                throw;
+            }
 
-                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-                // context.Database.Migrate();
-                if (!context.Clients.Any())
-                {
-                    foreach (var client in Config.Clients)
-                    {
-                        context.Clients.Add(client.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
+            if (!configContext.Clients.Any())
+            {
+                foreach (var client in Config.Clients) 
+                    configContext.Clients.Add(client.ToEntity());
+                    
+                configContext.SaveChanges();
+            }
 
-                if (!context.IdentityResources.Any())
-                {
-                    foreach (var resource in Config.IdentityResources)
-                    {
-                        context.IdentityResources.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
+            if (!configContext.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources) 
+                    configContext.IdentityResources.Add(resource.ToEntity());
+                    
+                configContext.SaveChanges();
+            }
 
-                if (!context.ApiScopes.Any())
-                {
-                    foreach (var resource in Config.ApiScopes)
-                    {
-                        context.ApiScopes.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
+            if (!configContext.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes) 
+                    configContext.ApiScopes.Add(resource.ToEntity());
+                    
+                configContext.SaveChanges();
             }
         }
     }
